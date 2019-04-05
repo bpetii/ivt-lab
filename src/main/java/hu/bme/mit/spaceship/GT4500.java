c838b5ea79dd0983640d2652c9cd2188338290e5
Bíró Péter <biro.peti09@gmail.com>
2019-04-05 09:24:54 +0200

    fix it

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,8 +77,10 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
+
         // try to fire both of the torpedo stores
         //TODO implement feature
+        firingSuccess=true;
 
         break;
     }
